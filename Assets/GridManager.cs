f6bc9dd193a1db962ac7d1e5e7077afde9bf6336
NSQTheBombBattle <elina@thebombbattle.com>
2025-03-18 17:49:59 +0800

    feat: basic working tetromino

---
+++ Assets/GridManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private GameObject blockPrefab;
+    public int width = 10;
+    public int height = 20;
+    public Transform[,] grid;
+    private float gridSizeScale;
+
+    private void Start()
+    {
+        InitGrid();
+        gridSizeScale = Mathf.Min(blockPrefab.GetComponent<SpriteRenderer>().bounds.size.x, blockPrefab.GetComponent<SpriteRenderer>().bounds.size.y);
+    }
+
+    private void InitGrid()
+    {
+        grid = new Transform[width, height];
+    }
+
+    public bool IsInsideGrid(Vector2 pos)
+    {
+        return pos.x >= 0 && pos.x < width * gridSizeScale && pos.y >= 0;
+    }
+
+    public bool IsGridOccupied(Vector2 pos)
+    {
+        float xPos = pos.x / gridSizeScale;
+        float yPos = pos.y / gridSizeScale;
+
+        return grid[(int)xPos, (int)yPos] != null;
+    }
+
+    public void CheckForLineClear()
+    {
+        for (int y = height - 1; y >= 0; y--)
+        {
+            if (IsLineComplete(y))
+            {
+                ClearLine(y);
+                MoveAllLinesDown(y);
+            }
+        }
+    }
+
+    bool IsLineComplete(int y)
+    {
+        for (int x = 0; x < width; x++)
+            if (grid[x, y] == null) return false;
+        return true;
+    }
+
+    void ClearLine(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+    void MoveAllLinesDown(int y)
+    {
+        for (int i = y; i < height - 1; i++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, i + 1] != null)
+                {
+                    grid[x, i] = grid[x, i + 1];
+                    grid[x, i].position += Vector3.down * gridSizeScale;
+                    grid[x, i + 1] = null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Tetromino.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    public GridManager gridManager;
+    private float fallTime = 0.8f; // Time before falling
+    private float previousTime;
+
+    void Update()
+    {
+        if (Time.time - previousTime > fallTime)
+        {
+            MoveDown();
+            previousTime = Time.time;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Move(Vector2.left);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) Move(Vector2.right);
+        if (Input.GetKeyDown(KeyCode.DownArrow)) MoveDown();
+        if (Input.GetKeyDown(KeyCode.UpArrow)) Rotate();
+    }
+
+    void Move(Vector2 direction)
+    {
+        transform.position += (Vector3)direction;
+        if (!ValidMove()) transform.position -= (Vector3)direction;
+    }
+
+    void MoveDown()
+    {
+        transform.position += Vector3.down;
+        if (!ValidMove())
+        {
+            transform.position -= Vector3.down;
+            AddToGrid();
+            //CheckForLineClear();
+            this.enabled = false;
+            //FindObjectOfType<Spawner>().SpawnNewTetromino();
+        }
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(0, 0, 90);
+        if (!ValidMove()) transform.Rotate(0, 0, -90);
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 pos = child.position;
+            if (!gridManager.IsInsideGrid(pos)) return false;
+            if (gridManager.IsGridOccupied(pos)) return false;
+        }
+        return true;
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 pos = child.position;
+            gridManager.grid[(int)pos.x, (int)pos.y] = child;
+        }
+    }
+}
+
