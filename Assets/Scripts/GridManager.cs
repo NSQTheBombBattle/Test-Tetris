5492751b8dda12fe2c56f7e47ce732274bff780b
NSQTheBombBattle <elina@thebombbattle.com>
2025-04-14 10:30:39 +0800

    chore: clean up unused codes

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,7 +5,6 @@ public class GridManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> tetrominoPrefabs;
     [SerializeField] private GameObject blockPrefab;
-    [SerializeField] private GameObject blockPrefab2;
     [SerializeField] private int gameOverHeight = 20;
     [SerializeField] private GameObject blockIndicatorPrefab;
     [SerializeField] private Transform blockIndicatorParent;
@@ -28,7 +27,7 @@ public class GridManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GeneratePuzzle2();
+            GeneratePuzzle();
         }
     }
 
@@ -60,14 +59,6 @@ public class GridManager : MonoBehaviour
         {
             Debug.Log("Game Over!");
         }
-    }
-
-    private void SpawnTetromino()
-    {
-        GameObject randomTetromino = tetrominoPrefabs[Random.Range(0, tetrominoPrefabs.Count)];
-        GameObject tetrominoInstance = Instantiate(randomTetromino);
-        tetrominoInstance.GetComponent<Tetromino>().gridManager = this;
-        tetrominoInstance.GetComponent<Tetromino>().InitTetromino(new Vector2Int(4, 20));
     }
 
     public bool IsInsideGrid(Vector2 posIndex)
@@ -102,14 +93,14 @@ public class GridManager : MonoBehaviour
         return false;
     }
 
-    bool IsLineComplete(int y)
+    private bool IsLineComplete(int y)
     {
         for (int x = 0; x < width; x++)
             if (grid[x, y] == null) return false;
         return true;
     }
 
-    void ClearLine(int y)
+    private void ClearLine(int y)
     {
         for (int x = 0; x < width; x++)
         {
@@ -118,7 +109,7 @@ public class GridManager : MonoBehaviour
         }
     }
 
-    void MoveAllLinesDown(int y)
+    private void MoveAllLinesDown(int y)
     {
         for (int i = y; i < height - 1; i++)
         {
@@ -143,69 +134,6 @@ public class GridManager : MonoBehaviour
         targetBlockLocation.Clear();
         occupiedBlockLocation.Clear();
 
-        float chanceToOccupied = 0.6f;
-        float yOffset = 0;
-        for (int j = 0; j < gridRowCount; j++)
-        {
-            float xOffset = 0;
-            for (int i = 0; i < gridColumnCount; i++)
-            {
-                List<(int, int)> targetBlockList = new List<(int, int)>();
-                int highestY = 0;
-                for (int y = gridSize - 1; y >= 0; y--)
-                {
-                    for (int x = 0; x < gridSize; x++)
-                    {
-                        if (Random.Range(0, 1f) <= chanceToOccupied && (y == gridSize - 1 || y >= highestY || targetBlockList.Contains((x, y + 1))))
-                        {
-                            GameObject targetBlockInstance = Instantiate(blockPrefab);
-                            targetBlockInstance.transform.position = new Vector2(x * gridSizeScale + xOffset, y * gridSizeScale + yOffset);
-                            targetBlockInstance.GetComponent<Block>().indexOffset = new Vector2Int(x, y);
-                            targetBlockLocation.Add(targetBlockInstance.transform);
-                            targetBlockList.Add((x, y));
-                            if (y > highestY)
-                            {
-                                highestY = y;
-                            }
-                        }
-                        else
-                        {
-                            if (y > highestY)
-                            {
-                                GameObject occupiedBlockInstance = Instantiate(blockPrefab);
-                                occupiedBlockInstance.transform.position = new Vector2(x * gridSizeScale + xOffset, y * gridSizeScale + yOffset);
-                                targetBlockLocation.Add(occupiedBlockInstance.transform);
-                                targetBlockList.Add((x, y));
-                                continue;
-                            }
-                            GameObject targetBlockInstance = Instantiate(blockPrefab2);
-                            targetBlockInstance.transform.position = new Vector2(x * gridSizeScale + xOffset, y * gridSizeScale + yOffset);
-                            targetBlockInstance.GetComponent<Block>().indexOffset = new Vector2Int(x + i * gridSize, y + j * gridSize);
-                            occupiedBlockLocation.Add(targetBlockInstance.transform);
-                        }
-                    }
-                }
-                xOffset += gridSize * gridSizeScale;
-            }
-            yOffset += gridSize * gridSizeScale;
-        }
-        AddBlockToGrid(Vector2Int.zero, occupiedBlockLocation);
-        for (int i = targetBlockLocation.Count - 1; i >= 0; i--)
-        {
-            Destroy(targetBlockLocation[i].gameObject);
-        }
-        targetBlockLocation.Clear();
-    }
-
-    private void GeneratePuzzle2()
-    {
-        for (int i = occupiedBlockLocation.Count - 1; i >= 0; i--)
-        {
-            Destroy(occupiedBlockLocation[i].gameObject);
-        }
-        targetBlockLocation.Clear();
-        occupiedBlockLocation.Clear();
-
         float yOffset = 0;
         for (int j = 0; j < gridRowCount; j++)
         {
@@ -221,11 +149,10 @@ public class GridManager : MonoBehaviour
                     {
                         if (!occuppiedIndex.Contains(new Vector2Int(x, y)))
                         {
-                            GameObject targetBlockInstance = Instantiate(blockPrefab2);
+                            GameObject targetBlockInstance = Instantiate(blockPrefab);
                             targetBlockInstance.transform.position = new Vector2(x * gridSizeScale + xOffset, y * gridSizeScale + yOffset);
                             targetBlockInstance.GetComponent<Block>().indexOffset = new Vector2Int(x + i * gridSize, y + j * gridSize);
                             occupiedBlockLocation.Add(targetBlockInstance.transform);
-                            Debug.Log("Spawn");
                         }
                     }
                 }
